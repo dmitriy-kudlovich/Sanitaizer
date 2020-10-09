5e4233e51de7b067890aaa7361070259556147d2
Dmitriy <carnagery@mail.ru>
2020-10-10 02:04:22 +0300

    about ready

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -13,111 +13,118 @@ const del = require("del");
 
 // Таск для сборки Pug-файлов
 gulp.task("pug", function (callback) {
-  return gulp
-    .src("./src/pug/pages/**/*.pug")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function (err) {
-          return {
-            title: "Pug",
-            sound: false,
-            message: err.message,
-          };
-        }),
-      })
-    )
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(gulp.dest("./build/"))
-    .pipe(browserSync.stream());
-  callback();
+    return gulp
+        .src("./src/pug/pages/**/*.pug")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(function (err) {
+                    return {
+                        title: "Pug",
+                        sound: false,
+                        message: err.message,
+                    };
+                }),
+            })
+        )
+        .pipe(
+            pug({
+                pretty: true,
+            })
+        )
+        .pipe(gulp.dest("./build/"))
+        .pipe(browserSync.stream());
+    callback();
 });
 
 // Таск для компиляции scss в css
 gulp.task("sass", function (callback) {
-  return gulp
-    .src("src/scss/main.scss")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function (err) {
-          return {
-            title: "Styles",
-            sound: false,
-            message: err.message,
-          };
-        }),
-      })
-    )
-    .pipe(sourcemaps.init())
-    .pipe(sassGlob())
-    .pipe(
-      sass({
-        indentType: "tab",
-        indentWidth: 1,
-        outputStyle: "expanded",
-      })
-    )
-    .pipe(gcmq())
-    .pipe(autoprefixer({ overrideBrowserslist: ["last 4 versions"] }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("./build/css/"))
-    .pipe(browserSync.stream());
-  callback();
+    return gulp
+        .src("src/scss/main.scss")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(function (err) {
+                    return {
+                        title: "Styles",
+                        sound: false,
+                        message: err.message,
+                    };
+                }),
+            })
+        )
+        .pipe(sourcemaps.init())
+        .pipe(sassGlob())
+        .pipe(
+            sass({
+                indentType: "tab",
+                indentWidth: 1,
+                outputStyle: "expanded",
+            })
+        )
+        .pipe(gcmq())
+        .pipe(autoprefixer({ overrideBrowserslist: ["last 4 versions"] }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./build/css/"))
+        .pipe(browserSync.stream());
+    callback();
 });
 
 // Копирование изображений
 gulp.task("copy:img", function (callback) {
-  return gulp.src("./src/img/**/*.*").pipe(gulp.dest("./build/img/"));
-  callback();
+    return gulp.src("./src/img/**/*.*").pipe(gulp.dest("./build/img/"));
+    callback();
 });
 
 // Копирование JS
 gulp.task("copy:js", function (callback) {
-  return gulp.src("./src/js/**/*.*").pipe(gulp.dest("./build/js/"));
-  callback();
+    return gulp.src("./src/js/**/*.*").pipe(gulp.dest("./build/js/"));
+    callback();
+});
+
+// Копирование libs
+gulp.task("copy:libs", function (callback) {
+    return gulp.src("./src/libs/**/*.*").pipe(gulp.dest("./build/libs/"));
+    callback();
 });
 
 gulp.task("watch", function () {
-  // Слежение за картинками и скриптами и обновление браузера
-  watch(
-    ["./build/js/**/*.*", "./build/img/**/*.*"],
-    gulp.parallel(browserSync.reload)
-  );
+    // Слежение за картинками и скриптами и обновление браузера
+    watch(
+        ["./build/js/**/*.*", "./build/img/**/*.*", "./build/libs/**/*.*"],
+        gulp.parallel(browserSync.reload)
+    );
 
-  // Слежение за scss c задержкой в 1 секунду
-  watch("./src/scss/**/*.scss", function () {
-    setTimeout(gulp.parallel("sass"), 1000);
-  });
+    // Слежение за scss c задержкой в 1 секунду
+    watch("./src/scss/**/*.scss", function () {
+        setTimeout(gulp.parallel("sass"), 1000);
+    });
 
-  // Слежение за pug и сборка
-  watch("./src/pug/**/*.pug", gulp.parallel("pug"));
+    // Слежение за pug и сборка
+    watch("./src/pug/**/*.pug", gulp.parallel("pug"));
 
-  // Слежение за картинками и скриптами, копирование их в build
-  watch("./src/img/**/*.*", gulp.parallel("copy:img"));
-  watch("./src/js/**/*.*", gulp.parallel("copy:js"));
+    // Слежение за картинками и скриптами, копирование их в build
+    watch("./src/img/**/*.*", gulp.parallel("copy:img"));
+    watch("./src/js/**/*.*", gulp.parallel("copy:js"));
+    watch("./src/libs/**/*.*", gulp.parallel("copy:libs"));
 });
 
 // Задача для старта сервера из папки build
 gulp.task("browser-sync", function () {
-  browserSync.init({
-    server: {
-      baseDir: "./build/",
-    },
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./build/",
+        },
+    });
 });
 
 gulp.task("clean:build", function () {
-  return del("./build/");
+    return del("./build/");
 });
 
 gulp.task(
-  "default",
-  gulp.series(
-    gulp.parallel("clean:build"),
-    gulp.parallel("sass", "pug", "copy:img", "copy:js"),
-    gulp.parallel("browser-sync", "watch")
-  )
+    "default",
+    gulp.series(
+        gulp.parallel("clean:build"),
+        gulp.parallel("sass", "pug", "copy:img", "copy:js", "copy:libs"),
+        gulp.parallel("browser-sync", "watch")
+    )
 );
